6e5b2c0db499d08bf34a0b82a4cf7bea3a4b9921
hhourani27 <h.hourany@gmail.com>
2020-12-28 15:26:19 +0100

    skynet2: optimize # of nodes

--- skynet2/skynet2.py
+++ skynet2/skynet2.py
@@ -23,28 +23,36 @@ node_counter = 0
 # State = (action,player,graph,gw_links,agentPos)
 # Action = gw to cut
 
+def get_gwps(gw_links):
+    return {gwp for gwp,gw in gw_links}
+
+def links_to_gw(gw_links, gwp):
+    count = 0
+    for a,b in gw_links:
+        if a == gwp:
+            count += 1
+    return count
+
 def terminal(state):
-    if len(state[3]) == 0: return True
-    if state[4] in gws : return True
+    action,player,graph,gw_links,agentPos = state
+    if len(gw_links) == 0: return 1
+    if agentPos in gws : return -1
+    if agentPos in get_gwps(gw_links) and links_to_gw(gw_links,agentPos) > 1: return -1
     
-def utility(state):
-    if len(state[3]) == 0: return 1
-    if state[4] in gws : return -1
+    return None
     
 def shortest_path(graph,start,goal):
     #verify cache
-    key = (start,goal) if start < goal else (goal,start)
+    key = (start,goal)
     if key in shortest_path_cache:
         return shortest_path_cache[key]
     
     frontier = deque([start])
     parent = {start:None}
     
-    found_goal = False
     while len(frontier) > 0:
         current = frontier.popleft()
         if current == goal:
-            found_goal = True
             break
         neighbors = graph[current].nonzero()[0]
         for neighbor in neighbors:
@@ -52,20 +60,41 @@ def shortest_path(graph,start,goal):
                 frontier.append(neighbor)
                 parent[neighbor] = current
                 
-    if found_goal is True:
+    distance = 1
         while True:
             prev_node = parent[current]
             if prev_node == start:
-                shortest_path_cache[key] = current
-                return current
+            shortest_path_cache[key] = (current,distance)
+            return (current,distance)
             current = prev_node
+        distance += 1
+        if current is None:
+            pass
+        
+
     else:
         return None
 
 def successors(state) :
     action,player,graph,gw_links,agentPos = state
+    gwps = get_gwps(gw_links)
+    
     successors = []
     if player == PLAYER:
+        # if agent is on a gwp, choose the link to the gw
+        if agentPos in gwps:
+            gwl = list(filter(lambda x: x[0]==agentPos,gw_links))[0]
+            gwp,gw = gwl
+            next_graph = graph.copy()
+            next_graph[gwp][gw] = False
+            next_graph[gw][gwp] = False
+
+            next_gw_links = gw_links.copy()
+            next_gw_links.remove(gwl)            
+            successors.append((gwl,AGENT,next_graph,next_gw_links,agentPos))
+
+        else:
+#        gw_links_sorted = sorted(list(gw_links),key=lambda x:gw_link_dangerosity(x,graph,agentPos) )
         for gwl in gw_links:
             gwp,gw = gwl
             
@@ -78,26 +107,25 @@ def successors(state) :
             successors.append((gwl,AGENT,next_graph,next_gw_links,agentPos))
 
     elif player == AGENT:
-        gwps = {gwp for gwp,gw in gw_links}
         if agentPos in gwps:
             neighbors = graph[agentPos].nonzero()[0]
             for n in neighbors:
                 if n in gws:
                     successors.append((n,PLAYER,graph,gw_links,n))
         else:
-            closest_nodes = {shortest_path(graph, agentPos, gwp) for gwp in gwps}
+            closest_nodes = {shortest_path(graph, agentPos, gwp)[0] for gwp in gwps}
             for n in closest_nodes:
                 successors.append((n,PLAYER,graph,gw_links,n))
     
-#    print('{} -> {}'.format(state,successors))
     return successors
     
 def max_value(state,alpha,beta) : 
     global node_counter
     node_counter += 1
     
-    if terminal(state):
-        return (utility(state),state[0])
+    terminal_value = terminal(state)
+    if terminal_value is not None:
+        return (terminal_value,state[0])
     v = -10
     best_action = None
     for s in successors(state):
@@ -113,8 +141,9 @@ def min_value(state,alpha,beta) :
     global node_counter
     node_counter += 1
     
-    if terminal(state):
-        return (utility(state),state[0])
+    terminal_value = terminal(state)
+    if terminal_value is not None:
+        return (terminal_value,state[0])
     v = 10
     best_action = None
     for s in successors(state):
